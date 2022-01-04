59196d94160949fb01223c3dff0b7b9fb7de670d
phikko <skeezze@gmail.com>
2022-01-04 22:43:49 +0100

    added CSRF token header

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,24 @@
 import axios from "axios";
 
-export default axios.create({
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest',
-    'Accept': 'application/json'
-  },
-  baseURL: 'http://php.test',
+const getCookie = (name: string) => {
+  return document.cookie.split('; ').reduce((r, v) => {
+    const parts = v.split('=')
+    return parts[0] === name ? decodeURIComponent(parts[1]) : r
+  }, '')
+}
+
+const instance = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
   withCredentials: true
 })
+
+axios.interceptors.request.use((config) => {
+  config.baseURL = 'http://127.0.0.1:8000'
+  config.withCredentials = true
+  if(config.headers?.common) {
+    config.headers['X-CSRFToken'] = getCookie('csrftoken')
+  }
+  return config
+});
+
+export default axios
